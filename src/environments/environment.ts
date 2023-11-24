b7716715969e3cbe1813c0f6874e88d76c307d66
Benjamin Tucker <benjamint@bsquare.com>
2023-11-24 18:45:12 +0000

    Correct the origin for backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  gateway: '../',
+  gateway: '../backend',
   callback: 'callback',
   domain: 'dev-otm8dxsmwyxedtdp.us.auth0.com',
   clientId: 'DA6yufqbxlwhiE0QdwaTqVGkAf6iJ4G3',
