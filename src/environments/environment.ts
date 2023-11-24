71ff3eedaf94aab03b2c2e8a1f9bba9770a06327
Benjamin Tucker <benjamint@bsquare.com>
2023-11-24 16:57:59 +0000

    Point to the origin for backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  gateway: 'http://localhost:13000',
-  callback: 'http://localhost:4200/callback',
+  gateway: '../',
+  callback: 'callback',
   domain: 'dev-otm8dxsmwyxedtdp.us.auth0.com',
   clientId: 'DA6yufqbxlwhiE0QdwaTqVGkAf6iJ4G3',
   audience: 'https://dev-otm8dxsmwyxedtdp.us.auth0.com/api/v2/'  /* Unique identifier for the API. This value will be used as the audience parameter on authorization calls */
